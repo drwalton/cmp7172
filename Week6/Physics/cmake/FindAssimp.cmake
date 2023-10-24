801ae70fdb9936f02e66a5ea3b42952d8ba6e0ee
David R. Walton <drwalton@users.noreply.github.com>
2023-10-24 10:26:14 +0100

    Fix assimp cmake

--- Week6/Physics/cmake/FindAssimp.cmake
+++ Week6/Physics/cmake/FindAssimp.cmake
@@ -1,25 +1,25 @@
 find_path(Assimp_INCLUDE_DIR
         assimp/BaseImporter.h
     HINTS
-        ${PROJECT_SOURCE_DIR}/3rdParty/assimp/include
-        ${PROJECT_SOURCE_DIR}/../3rdParty/assimp/include
-        ${PROJECT_SOURCE_DIR}/../../3rdParty/assimp/include
+        ${PROJECT_SOURCE_DIR}/3rdParty/assimp-5.2.5/include
+        ${PROJECT_SOURCE_DIR}/../3rdParty/assimp-5.2.5/include
+        ${PROJECT_SOURCE_DIR}/../../3rdParty/assimp-5.2.5/include
     )
 
 find_library(Assimp_LIBRARY
-        assimp-vc142-mtd
+        assimp-vc143-mtd
     HINTS
-        ${PROJECT_SOURCE_DIR}/3rdParty/assimp/lib/x64
-        ${PROJECT_SOURCE_DIR}/../3rdParty/assimp/lib/x64
-        ${PROJECT_SOURCE_DIR}/../../3rdParty/assimp/lib/x64
+        ${PROJECT_SOURCE_DIR}/3rdParty/assimp-5.2.5/lib/x64
+        ${PROJECT_SOURCE_DIR}/../3rdParty/assimp-5.2.5/lib/x64
+        ${PROJECT_SOURCE_DIR}/../../3rdParty/assimp-5.2.5/lib/x64
     )
 
 find_path(Assimp_DLL_DIR
-        assimp-vc142-mtd.dll
+        assimp-vc143-mtd.dll
     HINTS
-        ${PROJECT_SOURCE_DIR}/3rdParty/assimp/lib/x64
-        ${PROJECT_SOURCE_DIR}/../3rdParty/assimp/lib/x64
-        ${PROJECT_SOURCE_DIR}/../../3rdParty/assimp/lib/x64
+        ${PROJECT_SOURCE_DIR}/3rdParty/assimp-5.2.5/lib/x64
+        ${PROJECT_SOURCE_DIR}/../3rdParty/assimp-5.2.5/lib/x64
+        ${PROJECT_SOURCE_DIR}/../../3rdParty/assimp-5.2.5/lib/x64
     )
 
 set(Assimp_INCLUDE_DIRS ${Assimp_INCLUDE_DIR})
